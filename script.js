36d588ce14cd5515ef9f5c728dd156e72614d5e8
Suraj Kumar <surajkumar620103@gmail.com>
2025-11-23 22:16:26 +0530

    Refactor food handling logic in render function; ensure food is displayed and repositioned correctly

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const blockWidth = 50
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
-const food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
 
 const blocks = []
 let IntervalId = null
@@ -26,6 +26,7 @@ for (let row = 0; row < rows; row++) {
 
 function render() {
     let head = null;
+    blocks[`${food.x}-${food.y}`].classList.add("food")
 
     if (direction === "left") {
         head = { x: snake[0].x, y: snake[0].y - 1 }
@@ -42,22 +43,29 @@ function render() {
         clearInterval(IntervalId)
     }
 
+    if (head.x == food.x && head.y == food.y) {
+        blocks[`${food.x}-${food.y}`].classList.remove("food")
+        food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+        blocks[`${food.x}-${food.y}`].classList.add("food")
+
+        snake.unshift(head)
+
+    }
+
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
     })
 
+    snake.unshift(head)
+    snake.pop()
+
     snake.forEach(segment => {
         blocks[`${segment.x}-${segment.y}`].classList.add("fill")
     })
-
-    snake.unshift(head)
-    snake.pop()
 }
 
 IntervalId = setInterval(() => {
-
     render()
-
 }, 500)
 
 addEventListener("keydown", (event) => {
