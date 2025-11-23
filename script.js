68f5ee68be7451a83ff2eee9b916ba736ca33298
Suraj Kumar <surajkumar620103@gmail.com>
2025-11-23 15:33:12 +0530

    Refactor snake movement logic and add game over alert; enhance styling for food blocks

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const blockWidth = 50
 
 const cols = Math.floor(board.clientWidth / blockWidth)
 const rows = Math.floor(board.clientHeight / blockHeight)
+const food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
 
 const blocks = []
-const snake = [{
-    x: 1, y: 3
-}, {
-    x: 1, y: 4
-}, {
-    x: 1, y: 5
-}]
+let IntervalId = null
+let direction = "right"
+const snake = [
+    { x: 1, y: 3 },
+    { x: 1, y: 4 },
+]
 
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
@@ -25,5 +25,51 @@ for (let row = 0; row < rows; row++) {
 }
 
 function render() {
-    
+    let head = null;
+
+    if (direction === "left") {
+        head = { x: snake[0].x, y: snake[0].y - 1 }
+    } else if (direction === "right") {
+        head = { x: snake[0].x, y: snake[0].y + 1 }
+    } else if (direction === "down") {
+        head = { x: snake[0].x + 1, y: snake[0].y }
+    } else if (direction === "up") {
+        head = { x: snake[0].x - 1, y: snake[0].y }
+    }
+
+    if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+        alert("Game Over")
+        clearInterval(IntervalId)
+    }
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
+    })
+
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+
+    snake.unshift(head)
+    snake.pop()
 }
+
+IntervalId = setInterval(() => {
+
+    render()
+
+}, 500)
+
+addEventListener("keydown", (event) => {
+    if (event.key == "ArrowUp") {
+        direction = "up"
+    } else if (event.key == "ArrowDown") {
+        direction = "down"
+    } else if (event.key == "ArrowLeft") {
+        direction = "left"
+    }
+    else if (event.key == "ArrowRight") {
+        direction = "right"
+    }
+
+})
